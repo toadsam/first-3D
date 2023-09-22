1f4ebd84c93d962b959b1461e5a915f7a4080fcc
toadsam <75034052+toadsam@users.noreply.github.com>
2023-09-23 01:56:56 +0900

    [ADD] 유니티 에셋 추가 (-재훈)
    유니티에셋추가

---
+++ Assets/Scrips/Follow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset; //기본 카메라의 값이 들어있다. 플레이어와 일정한 거리를 유지하기 위해서이다.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
