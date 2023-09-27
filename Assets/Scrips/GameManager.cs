b378bb8929b975626ad656f1cbd380c12d381186
toadsam <75034052+toadsam@users.noreply.github.com>
2023-09-27 14:23:04 +0900

    [ADD] 캐릭터 추가(-재훈)
    캐릭터 추가

--- Assets/Scrips/GameManager.cs
+++ Assets/Scrips/GameManager.cs
@@ -1,16 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject bear;
-    public static int bearNum;  //곰돌이 수
-    public Transform respwanPos;
+    public  int bearNum;  //곰돌이 수
+    public Transform bearRespwanPos;
+
+
+    
     public Player player;
     float scale;
 
-    private static GameManager Instance;  //싱글톤 진행
+    public static GameManager Instance;  //싱글톤 진행
 
 
     // Start is called before the first frame update
@@ -26,30 +29,40 @@ public class GameManager : MonoBehaviour
         bearNum = 0;
     }
 
+    private void Start()
+    {
+        BearRespwan();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        BearRespwan();
     }
 
     void BearRespwan()
     {
-        if (bearNum == 0)
+        if (bearNum < 1)
         {
-            scale = Random.Range(1F, 5F);
-            GameObject bears = Instantiate(bear, respwanPos.position, Quaternion.identity);
+
+            scale = Random.Range(1F, 3F);
+            GameObject bears = Instantiate(bear, bearRespwanPos.position, Quaternion.identity);
             bears.GetComponent<NPC>().player = player;
             bears.transform.localScale = new Vector3(scale, scale, scale);
             bearNum++;
+
         }
     }
 
     public void BearDie()
     {
+        
         bearNum--;
     }
+   
+    
 }
 
-
+//이건 스폰매니저로 만들어야할 듯
 //게임 메니저 싱글톤화 진행하고 어왜이크에 있는거 다 메서드로 바꾸고
 // 타입을 선언해서 각 몬스터마다 어떻게 해야하는지 보여주기
