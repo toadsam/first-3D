52d9ca15f7819a6c419bf3f6b2f1d07dead19701
toadsam <75034052+toadsam@users.noreply.github.com>
2023-10-25 19:27:10 +0900

    [ADD] 파티클 시스템 (-재훈)
    파티클 시스템

---
+++ Assets/Scrips/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Player player;
+    public ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "Player")
+        {
+            // playerConditions.health.curValue -= 1f;
+            //  Player.health -= 2;
+            //  Debug.Log("ºÒ·Î¸ÂÀºÃ¼·Â " + Player.health);
+            Debug.Log("나나나난");
+        }
+    }
+}
