1285fbe0db06652ab08059b34ec88013a6f4e8c9
toadsam <75034052+toadsam@users.noreply.github.com>
2023-10-29 03:53:37 +0900

    [ADD] 가시기믹 구현 (-재훈)
    가시기믹 구현

--- Assets/Scrips/Thorn.cs
+++ Assets/Scrips/Thorn.cs
@@ -7,23 +7,37 @@ public class Thorn : MonoBehaviour
     public Transform pos1;
     public Transform pos2;
     public Transform ThornPos;
+    int count;
     // Start is called before the first frame update
     void Start()
     {
-        ThornPos.position = pos1.position;
+        ThornPos.position = pos2.position;
+        count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+       // ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos2.position, 0.1f);
         Debug.Log(ThornPos.position);
-        if (ThornPos.position == pos1.position)
+
+        if (count == 0)
         {
-            ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos2.position, 1);
+            ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos1.position, 0.15f);
+            if(ThornPos.position == pos1.position) 
+            {
+                count++;
+            }
+            
         }
-        else if (ThornPos.position == pos2.position)
+        if (count == 1)
         {
-            ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos1.position, 1);
+            ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos2.position, 0.01f);
+            if (ThornPos.position == pos2.position)
+            {
+                count--;
+            }
+            
         }
     }
 }
