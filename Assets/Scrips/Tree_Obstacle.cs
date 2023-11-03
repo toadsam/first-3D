6b8eceb6f96ab0418d33d0eddd14fe26aae81704
toadsam <75034052+toadsam@users.noreply.github.com>
2023-11-03 13:21:30 +0900

    [ADD] 회전 장애물 추가 (-재훈)
    회전 장애물 추가

---
+++ Assets/Scrips/Tree_Obstacle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree_Obstacle : MonoBehaviour
+{
+    [Header("Rotation Object")]
+    public GameObject Center;
+    public GameObject Rotation_right;
+    public GameObject Rotation_left;
+
+    [Header("Rotation")]
+    public int rotateSpeed;
+    public float speedUpTime;
+    public int rotateSpeedUp;
+    private float curTime;
+    //public float curTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate(); 
+        SpeedUp();
+    }
+
+    private void Rotate()
+    {
+        Center.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+        Rotation_left.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+        Rotation_right.transform.Rotate(Vector3.up * Time.deltaTime * -rotateSpeed);
+    }
+
+    private void SpeedUp()
+    {
+        curTime += Time.deltaTime;
+        Debug.Log(curTime);
+        if(curTime > speedUpTime)
+        {
+            rotateSpeed += rotateSpeedUp;
+            curTime = 0;
+        }
+    }
+}
