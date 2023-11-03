9a970da8cbd09bb2c84855de06c63311e671f5fc
toadsam <75034052+toadsam@users.noreply.github.com>
2023-11-03 20:59:22 +0900

    [ADD]  장애물 추가 (-재훈)
     장애물 추가

---
+++ Assets/Scrips/Hammer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    [Header("Rotation Object")]
+    public GameObject Center;
+    public GameObject Rotation_right;
+    public GameObject Rotation_left;
+
+    [Header("Rotation")]
+    public int rotateSpeed;
+    public float speedUpTime;
+    public int rotateSpeedUp;
+    private float curTime;
+    //public float curTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        SpeedUp();
+    }
+
+    private void Rotate()
+    {
+        //Center.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+        //transform.localRotation = Quaternion.Euler(0, 40f, 0);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+
+    }
+
+    private void SpeedUp()
+    {
+        curTime += Time.deltaTime;
+        Debug.Log(curTime);
+        if (curTime > speedUpTime)
+        {
+            rotateSpeed += rotateSpeedUp;
+            curTime = 0;
+        }
+    }
+}
---
+++ Assets/Scrips/RotationObstacle.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObstacle : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath _waypointPath;
+
+    [SerializeField]
+    private float _speed;
+
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
+
+    public int rotateSpeed;
+
+    public GameObject RotationObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = _elapsedTime / _timeToWaypoint;
+        elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage);//한게에 스무딩이 추가된다
+        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+        //transform.rotation = Quaternion.Lerp(_previousWaypoint.rotation, _targetWaypoint.rotation, elapsedPercentage); //재미를 주기 위해서 일단 회전값도 한번 넣어봄
+        if (elapsedPercentage >= 1)
+        {
+            TargetNextWaypoint();
+        }
+    }
+    private void Update()
+    {
+        RotationObject.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
+    }
+
+    private void TargetNextWaypoint()
+    {
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+        _timeToWaypoint = distanceToWaypoint / _speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("adw");
+            other.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("adw");
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.SetParent(null);
+        }
+    }
+
+}
