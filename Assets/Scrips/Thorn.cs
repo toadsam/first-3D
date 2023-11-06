811fecb13f4cafaf9a8531377110e95747455e19
toadsam <75034052+toadsam@users.noreply.github.com>
2023-11-06 23:00:27 +0900

    [ADD] 스크립트 추가 (-재훈)
    스크립트 추가

--- Assets/Scrips/Thorn.cs
+++ Assets/Scrips/Thorn.cs
@@ -5,45 +5,27 @@ using UnityEngine;
 public class Thorn : MonoBehaviour
 {
     [Header("Object Pos")]
-    public Transform pos1;
-    public Transform pos2;
-    public Transform ThornPos;
+    private Transform _pos1;
+    private Transform _pos2;
+    private Transform _ThornPos;
 
     [Header("Object Time")]
-    public float pos1Time;
-    public float pos2Time;
+    [SerializeField]public float _pos1Time;
+    [SerializeField] public float _pos2Time;
     // Start is called before the first frame update
     private int count;
     void Start()
     {
-        ThornPos.position = pos2.position;
-        count = 0;
+        _pos1 = transform.GetChild(0).transform;
+        _pos2 = transform.GetChild(1).transform;
+        _ThornPos = transform.GetChild(2).transform;
+        _ThornPos.position = _pos2.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos2.position, 0.1f);
-       // Debug.Log(ThornPos.position);
-
-        if (count == 0)
-        {
-            ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos1.position, pos1Time);//0.15f
-            if (ThornPos.position == pos1.position) 
-            {
-                count++;
-            }
-            
-        }
-        if (count == 1)
-        {
-            ThornPos.position = Vector3.MoveTowards(ThornPos.position, pos2.position, pos2Time);// 0.01f
-            if (ThornPos.position == pos2.position)
-            {
-                count--;
-            }
-            
-        }
+        ThornMove();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -51,6 +33,28 @@ public class Thorn : MonoBehaviour
         if (other.tag == "Player")
         {
             Debug.Log("가시에 닿았습니다");
+        }
+    }
+
+    private void ThornMove()
+    {
+        if (count == 0)
+        {
+            _ThornPos.position = Vector3.MoveTowards(_ThornPos.position, _pos1.position, 0.25f);//0.15f_pos1Time
+            if (_ThornPos.position == _pos1.position)
+            {
+                count++;
+            }
+
+        }
+        if (count == 1)
+        {
+            _ThornPos.position = Vector3.MoveTowards(_ThornPos.position, _pos2.position, 0.08f);// 0.01f_pos2Time
+            if (_ThornPos.position == _pos2.position)
+            {
+                count--;
+            }
+
         }
     }
 }
